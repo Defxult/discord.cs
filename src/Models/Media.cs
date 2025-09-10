98ad32f52402255d9f43718fe5e7b3c7e8ce1549
Defxult <52070034+Defxult@users.noreply.github.com>
2025-09-09 21:43:26 -0400

    Add model `Media`

---
+++ src/Models/Media.cs
@@ -0,0 +1,49 @@
+using Discord.Utility;
+using Discord.Net;
+
+namespace Discord.Models;
+
+/// <summary>
+/// Represents a Discord CDN.
+/// </summary>
+public class Media : Downloadable, IEquatable<Media>
+{
+    /// <summary>
+    /// The hash value of the media.
+    /// </summary>
+    public readonly string Hash;
+
+    #region API Separated
+
+    /// <summary>
+    /// Whether the media is animated.
+    /// </summary>
+    public readonly bool IsAnimated;
+
+    #endregion
+
+    internal Media(string hash, string cdnHashUrl)
+    {
+        if (cdnHashUrl.StartsWith('/'))
+        {
+            Hash = hash;
+            IsAnimated = hash.StartsWith("a_");
+            Url = ApiRoute.Cdn.GetDescription() + cdnHashUrl + GetImageType();
+        }
+        else
+            throw new ArgumentException($"parameter {nameof(cdnHashUrl)} must start with '/'");
+    }
+    
+    public override bool Equals(object? other) => other is Media media && Equals(media);
+    public bool Equals(Media? other) => Url == other?.Url;
+    public override int GetHashCode() => Url.GetHashCode();
+
+    /// <summary>
+    /// Returns the URL.
+    /// </summary>
+    public override string ToString() =>
+        Url;
+    
+    private string GetImageType() =>
+        IsAnimated ? ".gif" : ".png";
+}
